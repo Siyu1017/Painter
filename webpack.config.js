0b4bbe82f463098d11868d82f8f68d95982cd319
Siyu <98018258+Siyu1017@users.noreply.github.com>
2024-04-02 13:21:43 +0000

    Update

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const WebpackObfuscator = require('webpack-obfuscator');
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+        index: ['./draw.js', './draw.css']
+    },
+    mode: process.env.NODE_ENV,
+    devtool: 'hidden-source-map',
+    output: {
+        filename: '[name].[contenthash].js',
+        hashDigestLength: 6
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+        ],
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserWebpackPlugin()],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        }),
+        new HtmlWebpackPlugin({
+            chunks: ["index"],
+            filename: "index.html",
+            template: './draw.html'
+        }),
+        new WebpackObfuscator({
+            rotateUnicodeArray: true
+        })
+    ],
+}
